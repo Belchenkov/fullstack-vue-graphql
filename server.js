42f725faa2780696ecaf055eb5181125bb3a2c55
Aleksey Belchenkov <u608110@gmail.com>
2020-12-03 13:57:10 +0300

    Connect to DB

--- server.js
+++ server.js
@@ -1,44 +1,32 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const todos = [
-    { task: 'Wash car', completed: false },
-    { task: 'Clean room', completed: true }
-];
+require('dotenv').config({ path: '.env' });
 
 const typeDefs = gql`
-  type Todo {
-      task: String
-      completed: Boolean
-  } 
-  
-  type Query {
-      getTodos: [Todo]
-  }
-  
-  type Mutation {
-      addTodo(task: String!, completed: Boolean!): Todo
-  }
+    type Todo {
+        task: String
+        completed: Boolean
+    }
+
+    type Query {
+        getTodos: [Todo]
+    }
 `;
 
-const resolvers = {
-    Query: {
-        getTodos: () => todos
-    },
-    Mutation: {
-        addTodo: (_, { task, completed }) => {
-            const todo = { task, completed };
-            todos.push(todo);
-
-            return todo;
-        }
-    }
-};
-
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    typeDefs
 });
 
-server.listen().then(({ url }) => {
-    console.log(`Server listening on ${url}`)
-});
+mongoose
+    .connect(process.env.MONGO_URI,  {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('DB Connected...');
+        server.listen().then(({ url }) => {
+            console.log(`Server listening on ${url}`);
+        });
+    })
+    .catch(err => console.error(err));
